2a63097bee75432750e0167ff75ea81278284327
simonxnm <sinanyildiz006@gmail.com>
2024-10-30 09:49:23 +0100

    v2

--- src/Runner.java
+++ src/Runner.java
@@ -3,5 +3,9 @@ public class Runner {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        //version2
+
+        System.out.println("version 2");
     }
 }
